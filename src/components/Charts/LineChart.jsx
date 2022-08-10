cbd3b8c227f62ce9991c44712c828982be8e00d5
Akinyele Akindoyin <doyinakinyele@yahoo.com>
2022-08-10 02:59:27 -0700

    init

---
+++ src/components/Charts/LineChart.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  LineSeries,
+  DateTime,
+  Legend,
+  Tooltip,
+} from '@syncfusion/ej2-react-charts';
+
+import {
+  lineCustomSeries,
+  LinePrimaryYAxis,
+  LinePrimaryXAxis,
+} from '../../data/dummy';
+import { useStateContext } from '../../contexts/contextProvider';
+
+const LineChart = () => {
+  const { currentMode } = useStateContext();
+
+  return (
+    <ChartComponent
+      id='line-Chart'
+      height='420px'
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373e' : '#fff'}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
+};
+
+export default LineChart;
